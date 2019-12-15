2c0c2c9bdc360e51c69bd1e9e53b0f843b9d1661
Dan <danandreist@gmail.com>
2019-12-15 21:41:02 +0200

    :zap:

--- models/chat.go
+++ models/chat.go
@@ -6,6 +6,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Chat struct {
 	ID           primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
 	FromUserName string             `json:"from_user_name" bson:"from_user_name,omitempty"`
+	FromUserPic  string             `json:"from_user_pic" bson:"from_user_pic,omitempty"`
+	ToUserPic    string             `json:"to_user_pic" bson:"to_user_pic,omitempty"`
 	FromUserID   string             `json:"from_user_id" bson:"from_user_id,omitempty"`
 	ToUserID     string             `json:"to_user_id" bson:"to_user_id,omitempty"`
 	ToUserName   string             `json:"to_user_name" bson:"to_user_name,omitempty"`
